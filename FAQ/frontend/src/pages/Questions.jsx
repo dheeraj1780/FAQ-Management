af16e1b9dd4d0924420f6ed002045a65b976e620
dheeraj <jdeeran2004@gmail.com>
2025-01-03 01:19:54 +0530

    dummy pages

---
+++ FAQ/frontend/src/pages/Questions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const questions = () => {
+  return <div></div>;
+};
+
+export default questions;
---
+++ FAQ/frontend/src/pages/ViewQuestion.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ViewQuestion = () => {
+  return <div></div>;
+};
+
+export default ViewQuestion;
