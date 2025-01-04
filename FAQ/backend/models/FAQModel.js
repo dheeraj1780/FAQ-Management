b96cb902e804e7b2b0f8b55144fe65c3bdee9756
dheeraj <jdeeran2004@gmail.com>
2025-01-04 18:23:55 +0530

    models

---
+++ FAQ/backend/models/FAQModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const FAQModel = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    answer: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("FAQ", FAQModel);
