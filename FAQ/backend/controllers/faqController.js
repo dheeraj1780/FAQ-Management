e48a71f1f422ddfb3b6e4087684d1221fb72d2c2
dheeraj <jdeeran2004@gmail.com>
2025-01-04 21:26:29 +0530

    faqcontroller edit

--- FAQ/backend/controllers/faqController.js
+++ FAQ/backend/controllers/faqController.js
@@ -1,30 +1,30 @@
 import mongoose from "mongoose";
-import FAQ from "../models/FAQModel";
+import FAQModel from "../models/FAQModel";
 import { StatusCodes } from "http-status-codes";
 
 export const createFAQ = async (res, req) => {
-  const faq = await FAQ.create(req.body);
+  const faq = await FAQModel.create(req.body);
   res.status(StatusCodes.OK).json({ faq });
 };
 
 export const getFAQ = async (req, res) => {
-  const faq = await FAQ.findById(req.params.id);
+  const faq = await FAQModel.findById(req.params.id);
   res.status(StatusCodes.OK).json({ faq });
 };
 
 export const updateFAQ = async (req, res) => {
-  const updatedFAQ = await FAQ.findByIdAndUpdate(req.params.id, req.body, {
+  const updatedFAQ = await FAQModel.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
   res.status(StatusCodes.OK).json({ msg: "faq modified", faq: updatedFAQ });
 };
 
 export const deleteFAQ = async (req, res) => {
-  const removedFAQ = await FAQ.findByIdAndDelete(req.params.id);
+  const removedFAQ = await FAQModel.findByIdAndDelete(req.params.id);
   res.status(StatusCodes.OK).json({ msg: "faq deleted", faq: removedFAQ });
 };
 
 export const getAllFAQ = async (res, req) => {
-  const getall = await FAQ.find();
+  const getall = await FAQModel.find();
   res.status(StatusCodes.OK).json({ getall });
 };
