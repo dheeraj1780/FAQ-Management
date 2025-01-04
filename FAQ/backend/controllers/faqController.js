0b40783409fb97969f11d1aba9c0b66239a2b138
Navyapriyalal <navyapriyalal2004@gmail.com>
2025-01-04 21:56:38 +0530

    faq controller

---
+++ FAQ/backend/controllers/faqController.js
@@ -0,0 +1,29 @@
+import FAQModel from "../models/FAQModel.js";
+import { StatusCodes } from "http-status-codes";
+
+export const createFAQ = async (res, req) => {
+  const faq = await FAQModel.create(req.body);
+  res.status(StatusCodes.OK).json({ faq });
+};
+
+export const getFAQ = async (req, res) => {
+  const faq = await FAQModel.findById(req.params.id);
+  res.status(StatusCodes.OK).json({ faq });
+};
+
+export const updateFAQ = async (req, res) => {
+  const updatedFAQ = await FAQModel.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(StatusCodes.OK).json({ msg: "faq modified", faq: updatedFAQ });
+};
+
+export const deleteFAQ = async (req, res) => {
+  const removedFAQ = await FAQModel.findByIdAndDelete(req.params.id);
+  res.status(StatusCodes.OK).json({ msg: "faq deleted", faq: removedFAQ });
+};
+
+export const getAllFAQ = async (res, req) => {
+  const getall = await FAQModel.find();
+  res.status(StatusCodes.OK).json({ getall });
+};
