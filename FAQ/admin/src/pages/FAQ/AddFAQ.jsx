82b376d46871663079ad892e78b6df7972810e2d
dheeraj <jdeeran2004@gmail.com>
2025-01-10 17:18:26 +0530

    editor

--- FAQ/admin/src/pages/FAQ/AddFAQ.jsx
+++ FAQ/admin/src/pages/FAQ/AddFAQ.jsx
@@ -1,5 +1,34 @@
 import Wrapper from "../../assets/wrappers/FAQForm";
+import React, { useEffect, useRef } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
 const AddFAQ = () => {
+  const editorRef = useRef(null);
+  useEffect(() => {
+    if (!editorRef.current.quill) {
+      // Check if Quill is already initialized
+      const quill = new Quill(editorRef.current, {
+        theme: "snow",
+        modules: {
+          toolbar: [
+            [{ header: [1, 2, false] }],
+            ["bold", "italic", "underline", "strike"],
+            [{ list: "ordered" }, { list: "bullet" }],
+            ["link", "image"],
+            ["clean"],
+          ],
+        },
+      });
+      quill.on("text-change", () => {
+        const content = quill.root.innerHTML; // Get HTML content
+        editorRef.current.dataset.content = content; // Store content in ref
+      });
+
+      editorRef.current.quill = quill; // Store Quill instance in ref
+    }
+  }, []);
+
   return (
     <Wrapper>
       <form>
@@ -9,8 +38,7 @@ const AddFAQ = () => {
           <input type="text" id="question" placeholder="Enter the question" />
         </div>
         <div className="form-group">
-          <label htmlFor="answer">Answer</label>
-          <textarea id="answer" placeholder="Enter the answer" />
+          <div ref={editorRef} style={{ height: "300px" }}></div>
         </div>
         <div className="form-group">
           <label htmlFor="category">Category</label>
